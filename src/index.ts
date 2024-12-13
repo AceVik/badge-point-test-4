f01feaf3520a8dbd50b3e2f9a91a88588ef488e8
Viktor Machnik <viktor@machnik.consulting>
2024-12-13 19:44:09 +0100

    Version 2: Icon enum was fixed by removing DEFAULT entry to make it a pure string enum. Made the getUsersBadge function easier to read and future proofed by establishing badge rules.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
 import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
-
-const BRONZE_MIN = 5;
-const SILVER_MIN = 25;
-const GOLD_MIN = 50;
-
-const isBronze = (count: number) => count >= BRONZE_MIN && count < SILVER_MIN;
-const isSilver = (count: number) => count >= SILVER_MIN && count < GOLD_MIN;
-const isGold = (count: number) => count >= GOLD_MIN;
+import { badgeRules } from './badgeRules';
 
 export const getUsersBadge = (user: User): Icon | null => {
-  switch (true) {
-    case isBronze(user.solutionCount):
-      return Icon.BADGE_BRONZE;
-
-    case isSilver(user.solutionCount):
-        return Icon.BADGE_SILVER;
-
-    case isGold(user.solutionCount):
-        return Icon.BADGE_GOLD;
-
-    default:
-        return null;
+    for (const { min, badge } of badgeRules) {
+        if (user.solutionCount >= min) return badge;
     }
+
+    return null;
 };
